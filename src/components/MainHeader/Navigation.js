5d49499b58e989977b19b6e3775236c640b6c8c6
tusharkarlawar <tusharkarlawar95@gmail.com>
2023-07-11 18:31:52 +0530

    Context_API

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -11,29 +11,27 @@ const Navigation = (props) => {
       {(ctx) => {
         console.log(ctx);
         return (
-          
-        <nav className={classes.nav}>
-          <ul>
-            {ctx.isLoggedIn && (
-              <li>
-                <a href="/">Users</a>
-              </li>
-            )}
-            {ctx.isLoggedIn && (
-              <li>
-                <a href="/">Admin</a>
-              </li>
-            )}
-            {ctx.isLoggedIn && (
-              <li>
-                <button onClick={props.onLogout}>Logout</button>
-              </li>
-            )}
-          </ul>
-        </nav>
+          <nav className={classes.nav}>
+            <ul>
+              {ctx.isLoggedIn && (
+                <li>
+                  <a href="/">Users</a>
+                </li>
+              )}
+              {ctx.isLoggedIn && (
+                <li>
+                  <a href="/">Admin</a>
+                </li>
+              )}
+              {ctx.isLoggedIn && (
+                <li>
+                  <button onClick={props.onLogout}>Logout</button>
+                </li>
+              )}
+            </ul>
+          </nav>
         );
-      }};
-    
+      }}
     </AuthContext.Consumer>
   );
 };
